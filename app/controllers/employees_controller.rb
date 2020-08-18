3142de048596114df0bd997d6aa847f95426dd9d
JuanesLinux <juanesph@gmail.com>
2020-08-18 03:34:04 -0400

    modifications to employees controller and routes

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,17 +1,20 @@
 class EmployeesController < ApplicationController
   def index
+    @employees = Employee.all
   end
 
   def show
   end
 
   def new
+    @employee = Employee.new
   end
 
   def edit
   end
 
   def create
+    @employee = Employee.new(employee_params)
   end
 
   def update
@@ -19,4 +22,9 @@ class EmployeesController < ApplicationController
 
   def destroy
   end
+
+  private
+    def employee_params
+      params.require(:employee).permit(:name, :rut, :position, :birthday, :phone, :start_date, :end_date)
+    end
 end
